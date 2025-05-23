05cf645f1ac19b24e6b169c9f117d07e616098a2
Happke <alehappke@gmail.com>
2025-05-23 19:37:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('DOM carregado com sucesso.');
+  window.mostrar = function(selecao) {
+    console.log('Função mostrar chamada com:', selecao);
+    // Lógica para mostrar/esconder divs
+  };
+  // Outras funções...
+});
