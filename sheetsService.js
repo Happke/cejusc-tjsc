1478dd1cae2c7dfec4a85457fd535fef0a2d1a30
Happke <alehappke@gmail.com>
2025-05-27 18:17:30 -0300

    Create sheetsService.js

---
+++ sheetsService.js
@@ -0,0 +1,23 @@
+import { google } from 'googleapis';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const auth = new google.auth.JWT(
+  process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  null,
+  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  ['https://www.googleapis.com/auth/spreadsheets.readonly']
+);
+
+const sheets = google.sheets({ version: 'v4', auth });
+
+export async function getMetas() {
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SPREADSHEET_ID,
+    range: 'METAS!A2:C',
+  });
+
+  const values = res.data.values || [];
+  return values.filter(row => row[0] && row[1] && row[2] && !isNaN(row[2]));
+}
